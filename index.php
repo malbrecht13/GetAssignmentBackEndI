578787a9a2b4bf876a0c9b57c505042af75f016c
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2021-01-28 14:33:05 -0600

    Updated appearance and avoided errors with entering a non-numeric age

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
     $firstname = filter_input(INPUT_GET, 'first', FILTER_SANITIZE_STRING);
     $lastname = filter_input(INPUT_GET, 'last', FILTER_SANITIZE_STRING);
     $age = filter_input(INPUT_GET, 'age', FILTER_SANITIZE_NUMBER_INT);
-    $days = $age * 365;
-    $days_f = number_format($days);
+    if(is_numeric($age)) {
+        $days = $age * 365;
+        $days_f = number_format($days);
+    }
+
     $date = date('m/d/Y');
 ?>
 
@@ -13,14 +16,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
     <title>Get Assignment</title>
 </head>
 <body>
     <p class="float-right"> <?php echo "Today's date is ".$date."."; ?> </p>
     <main>
-        <h1> <?php include('./view/header.php'); ?> </h1>
-        <p> <?php include('./view/paragraph.php'); ?> </p>
+        <div id="central-div">
+            <h1> <?php include('./view/header.php'); ?> </h1>
+            <p> <?php include('./view/paragraph.php'); ?> </p>
+        </div>
     </main>
 </body>
 </html>
--- view/paragraph.php
+++ view/paragraph.php
@@ -1,5 +1,5 @@
 <?php
-    if(!empty($firstname) && !empty($lastname) && !empty($age) ) {
+    if(!empty($firstname) && !empty($lastname) && !empty($age) && is_numeric($age)) {
         echo "I am ".htmlspecialchars($age)." years old and ";
         if($age < 18) {
             echo 'I am not old enough to vote in the United States';
@@ -9,14 +9,13 @@
         echo "<br>That means I am at least ".$days_f." days old.";
     } else {
         if(empty($firstname)) {
-            echo "No first name was provided.<br>";
+            echo "You did not submit a firstname parameter.<br>";
         }
         if(empty($lastname)) {
-            echo "No last name was provided.<br>";
+            echo "You did not submit a lastname parameter.<br>";
         }
-        if(empty($age)) {
-            echo "No age was provided.";
+        if(empty($age) || !is_numeric($age)) {
+            echo "You did not submit a numeric age parameter.";
         }
     }
 ?>
-
